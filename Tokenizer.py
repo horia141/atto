af0082f8858418537076223d4d40807a5c43673b
Horia Coman <horia141@gmail.com>
2011-05-22 23:24:15 +0300

    Updated Tokenizer.
    * We now use the Stream module. The code is cleaner and more streamlined.

--- Tokenizer.py
+++ Tokenizer.py
@@ -3,184 +3,135 @@ import re
 import Stream
 
 class Token(object):
-    def __init__(self,text,abs_beg,abs_end,rel_beg,rel_end):
-        self.text = text
-        self.geometry = Utils.TextGeometry(abs_beg,abs_end,rel_beg[0],rel_beg[1],rel_end[0],rel_end[1])
+    def __init__(self,text,geometry):
+        self.__text = text
+        self.__geometry = geometry
 
     def __str__(self):
-        return 'Token \'' + self.text + '\''
+        return 'Token \'' + self.__text + '\''
 
-    def getText(self):
-        return self.text
+    def __repr__(self):
+        return 'Token(' + repr(self.__text) + ',' + repr(self.__geometry) + ')'
 
-    def getGeometry(self):
-        return self.geometry
+    @property
+    def text(self):
+        return self.__text
+
+    @property
+    def geometry(self):
+        return self.__geometry
 
 class CallBeg(Token):
     def __str__(self):
         return 'CallBeg'
 
+    def __repr__(self):
+        return 'CallBeg(' + repr(self.text) + ',' + repr(self.geometry) + ')'
+
 class CallEnd(Token):
     def __str__(self):
         return 'CallEnd'
+
+    def __repr__(self):
+        return 'CallEnd(' + repr(self.text) + ',' + repr(self.geometry) + ')'
 
 class Column(Token):
     def __str__(self):
         return 'Column'
 
+    def __repr__(self):
+        return 'Column(' + repr(self.text) + ',' + repr(self.geometry) + ')'
+
 class Dollar(Token):
     def __str__(self):
         return 'Dollar'
+
+    def __repr__(self):
+        return 'Dollar(' + repr(self.text) + ',' + repr(self.geometry) + ')'
+
+class Equal(Token):
+    def __str__(self):
+        return 'Equal'
+
+    def __repr__(self):
+        return 'Equal(' + repr(self.text) + ',' + repr(self.geometry) + ')'
 
 class Star(Token):
     def __str__(self):
         return 'Star'
 
+    def __repr__(self):
+        return 'Star(' + repr(self.text) + ',' + repr(self.geometry) + ')'
+
 class Plus(Token):
     def __str__(self):
         return 'Plus'
+
+    def __repr__(self):
+        return 'Plus(' + repr(self.text) + ',' + repr(self.geometry) + ')'
 
 class Symbol(Token):
     def __str__(self):
         return 'Symbol \'' + self.text + '\''
 
+    def __repr__(self):
+        return 'Symbol(' + repr(self.text) + ',' + repr(self.geometry) + ')'
+
 class FuncBeg(Token):
     def __str__(self):
         return 'FuncBeg'
+
+    def __repr__(self):
+        return 'FuncBeg(' + repr(self.text) + ',' + repr(self.geometry) + ')'
 
 class FuncEnd(Token):
     def __str__(self):
         return 'FuncEnd'
 
+    def __repr__(self):
+        return 'FuncEnd(' + repr(self.text) + ',' + repr(self.geometry) + ')'
+
 class DictBeg(Token):
     def __str__(self):
         return 'DictBeg'
+
+    def __repr__(self):
+        return 'DictBeg(' + repr(self.text) + ',' + repr(self.geometry) + ')'
 
 class DictEnd(Token):
     def __str__(self):
         return 'DictEnd'
 
-def tokenize(input_string):
-    def to_rel(pos):
-        abs = 0
-        row = 0
-        col = 0
+    def __repr__(self):
+        return 'DictEnd(' + repr(self.text) + ',' + repr(self.geometry) + ')'
 
-        while abs < pos:
-            if input_string[abs] == '\n':
-                row = row + 1
-                col = 0
-            else:
-                col = col + 1
-
-            abs = abs + 1
-
-        return (row,col)
-
+def tokenize(stream):
     tokens = []
-    pos = 0
+    parsers = [(CallBeg,re.compile(r'[(]')),
+               (CallEnd,re.compile(r'[)]')),
+               (Column,re.compile(r'[|]')),
+               (Dollar,re.compile(r'[$]')),
+               (Equal,re.compile(r'[=]')),
+               (Star,re.compile(r'[*]')),
+               (Plus,re.compile(r'[+]')),
+               (Symbol,re.compile(r'[a-zA-Z0-9-_]+')),
+               (Symbol,re.compile(r'[<]([^>]+)[>]')),
+               (FuncBeg,re.compile(r'[[]')),
+               (FuncEnd,re.compile(r'[]]')),
+               (DictBeg,re.compile(r'[{]')),
+               (DictEnd,re.compile(r'[}]')),
+               (None,re.compile(r'\s+'))]
 
-    callbeg = re.compile(r'[(]')
-    callend = re.compile(r'[)]')
-    column = re.compile(r'[|]')
-    dollar = re.compile(r'[$]')
-    star = re.compile(r'[*]')
-    plus = re.compile(r'[+]')
-    symbol0 = re.compile(r'[a-zA-Z0-9-_]+')
-    symbol1 = re.compile(r'[<]([^>]+)[>]')
-    funcbeg = re.compile(r'[[]')
-    funcend = re.compile(r'[]]')
-    dictbeg = re.compile(r'[{]')
-    dictend = re.compile(r'[}]')
-    space = re.compile(r'\s+')
+    while not stream.finished:
+        for tokenType,reobject in parsers:
+            res = stream.tryConsume(reobject)
 
-    while pos < len(input_string):
-        m = callbeg.match(input_string,pos)
-
-        if m:
-            tokens.append(CallBeg(m.group(),pos,m.end(),to_rel(pos),to_rel(m.end())))
-            pos = m.end()
-            continue
-
-        m = callend.match(input_string,pos)
-
-        if m:
-            tokens.append(CallEnd(m.group(),pos,m.end(),to_rel(pos),to_rel(m.end())))
-            pos = m.end()
-            continue
-
-        m = column.match(input_string,pos)
-
-        if m:
-            tokens.append(Column(m.group(),pos,m.end(),to_rel(pos),to_rel(m.end())))
-            pos = m.end()
-            continue
-        
-        m = dollar.match(input_string,pos)
-
-        if m:
-            tokens.append(Dollar(m.group(),pos,m.end(),to_rel(pos),to_rel(m.end())))
-            pos = m.end()
-            continue
-
-        m = star.match(input_string,pos)
-
-        if m:
-            tokens.append(Star(m.group(),pos,m.end(),to_rel(pos),to_rel(m.end())))
-            pos = m.end()
-            continue
-
-        m = plus.match(input_string,pos)
-
-        if m:
-            tokens.append(Plus(m.group(),pos,m.end(),to_rel(pos),to_rel(m.end())))
-            pos = m.end()
-            continue
-
-        m = symbol.match(input_string,pos)
-
-        if m:
-            if m.group(1):
-                tokens.append(Symbol(m.group(1),pos,m.end(),to_rel(pos),to_rel(m.end())))
-            else:
-                tokens.append(Symbol(m.group(2),pos,m.end(),to_rel(pos),to_rel(m.end())))
-            pos = m.end()
-            continue
-
-        m = funcbeg.match(input_string,pos)
-
-        if m:
-            tokens.append(FuncBeg(m.group(),pos,m.end(),to_rel(pos),to_rel(m.end())))
-            pos = m.end()
-            continue
-
-        m = funcend.match(input_string,pos)
-
-        if m:
-            tokens.append(FuncEnd(m.group(),pos,m.end(),to_rel(pos),to_rel(m.end())))
-            pos = m.end()
-            continue
-
-        m = dictbeg.match(input_string,pos)
-
-        if m:
-            tokens.append(DictBeg(m.group(),pos,m.end(),to_rel(pos),to_rel(m.end())))
-            pos = m.end()
-            continue
-
-        m = dictend.match(input_string,pos)
-
-        if m:
-            tokens.append(DictEnd(m.group(),pos,m.end(),to_rel(pos),to_rel(m.end())))
-            pos = m.end()
-            continue
-
-        m = space.match(input_string,pos)
-
-        if m:
-            pos = m.end()
-            continue
-
-        raise Exception('Invalid characters!')
+            if res:
+                if tokenType:
+                    tokens.append(tokenType(res[0],res[1]))
+                break
+        else:
+            raise Exception('Invalid character at line %d : column %d' % \
+                                (stream.relPos.row,stream.relPos.col))
 
     return tokens
