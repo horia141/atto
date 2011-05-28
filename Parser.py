a6bc406e60e57de67de0ebff297d875d390f217b
Horia Coman <horia141@gmail.com>
2011-05-28 17:56:32 +0300

    Upgraded Parser as well.
    * This concludes the read part of the new interpreter version.

--- Parser.py
+++ Parser.py
@@ -116,7 +116,7 @@ class Symbol(PsAtom):
 
     def __str__(self):
         if re.search('\s',self.__text):
-            return '{' + self.__text + '}'
+            return '`' + self.__text + '`'
         else:
             return self.__text
 
@@ -187,55 +187,54 @@ class Func(PsAtom):
     def body(self):
         return self.__body
 
-class DictKeysValue(object):
-    def __init__(self,keys,value):
-        assert(isinstance(keys,list))
-        assert(all(map(lambda x: isinstance(x,PsAtom),keys)))
+class DictKeyValue(object):
+    def __init__(self,key,value):
+        assert(isinstance(key,PsAtom))
         assert(isinstance(value,PsAtom))
 
-        self.__keys = map(lambda x: x.clone(),keys)
+        self.__key = key.clone()
         self.__value = value.clone()
 
     def __str__(self):
-        return ' '.join(map(str,self.__keys)) + ' ' + str(self.__value)
+        return str(self.__key) + ' ' + str(self.__value)
 
     def __repr__(self):
-        return 'Parser.DictKeysValue(' + repr(self.__keys) + ',' + \
+        return 'Parser.DictKeyValue(' + repr(self.__key) + ',' + \
                                         repr(self.__value) + ')'
 
     def clone(self):
-        return DictKeysValue(self.__keys,self.__value)
+        return DictKeyValue(self.__key,self.__value)
 
     @property
-    def keys(self):
-        return self.__keys
+    def key(self):
+        return self.__key
 
     @property
     def value(self):
         return self.__value
 
 class Dict(PsAtom):
-    def __init__(self,keysvalues,geometry):
-        assert(isinstance(keysvalues,list))
-        assert(all(map(lambda x: isinstance(x,DictKeysValue),keysvalues)))
+    def __init__(self,keyvalues,geometry):
+        assert(isinstance(keyvalues,list))
+        assert(all(map(lambda x: isinstance(x,DictKeyValue),keyvalues)))
 
         super(Dict,self).__init__(geometry)
 
-        self.__keysvalues = map(lambda x: x.clone(),keysvalues)
+        self.__keyvalues = map(lambda x: x.clone(),keyvalues)
 
     def __str__(self):
-        return '{' + '|'.join(map(str,self.__keysvalues)) + '}'
+        return '<' + ' '.join(map(str,self.__keyvalues)) + '>'
 
     def __repr__(self):
-        return 'Parser.Dict(' + '[' + ','.join(map(repr,self.__keysvalues)) + '],' + \
+        return 'Parser.Dict(' + '[' + ','.join(map(repr,self.__keyvalues)) + '],' + \
                                       repr(self.geometry) + ')'
 
     def clone(self):
-        return Dict(self.__keysvalues,self.geometry)
+        return Dict(self.__keyvalues,self.geometry)
 
     @property
-    def keysvalues(self):
-        return self.__keysvalues
+    def keyvalues(self):
+        return self.__keyvalues
 
 def parse(tokens,pos=0):
     assert(isinstance(tokens,list))
@@ -281,6 +280,26 @@ def parse(tokens,pos=0):
         return (pos,Lookup(names,geometry))
     elif isinstance(tokens[pos],Tokenizer.Symbol):
         return (pos+1,Symbol(tokens[pos].text,tokens[pos].geometry))
+    elif isinstance(tokens[pos],Tokenizer.BlockBeg):
+        init_geometry = tokens[pos].geometry
+        (pos,action) = parse(tokens,pos+1)
+        order_args = []
+        named_args = []
+
+        while not isinstance(tokens[pos],Tokenizer.BlockEnd):
+            (pos,new_arg) = parse(tokens,pos)
+
+            if isinstance(tokens[pos],Tokenizer.CallEqual):
+                (pos,value) = parse(tokens,pos+1)
+                named_args.append(CallNamedArg(new_arg,value))
+            else:
+                order_args.append(new_arg)
+
+        geometry = init_geometry.expandTo(tokens[pos].geometry)
+        body = Call(action,order_args,named_args,geometry)
+        return (pos+1,Func([],None,None,body,geometry))
+    elif isinstance(tokens[pos],Tokenizer.BlockEnd):
+        raise Exception('Invalid "}" character!')
     elif isinstance(tokens[pos],Tokenizer.FuncBeg):
         init_geometry = tokens[pos].geometry
         pos = pos + 1
@@ -323,29 +342,17 @@ def parse(tokens,pos=0):
     elif isinstance(tokens[pos],Tokenizer.DictBeg):
         init_geometry = tokens[pos].geometry
         pos = pos+1
-        keysvalues = []
+        keyvalues = []
 
         while not isinstance(tokens[pos],Tokenizer.DictEnd):
-            keysvalue = []
+            (pos,key) = parse(tokens,pos)
+            (pos,value) = parse(tokens,pos)
 
-            while not isinstance(tokens[pos],Tokenizer.Column) and \
-                  not isinstance(tokens[pos],Tokenizer.DictEnd):
-                (pos,newkey) = parse(tokens,pos)
-                keysvalue.append(newkey)
-
-            if len(keysvalue) < 2:
-                raise Exception('Invalid dictionary format!')
-
-            if isinstance(tokens[pos],Tokenizer.Column):
-                pos = pos + 1
-
-            keysvalues.append(DictKeysValue(keysvalue[:-1],keysvalue[-1]))
+            keyvalues.append(DictKeyValue(key,value))
 
         geometry = init_geometry.expandTo(tokens[pos].geometry)
-        return (pos+1,Dict(keysvalues,geometry))
+        return (pos+1,Dict(keyvalues,geometry))
     elif isinstance(tokens[pos],Tokenizer.DictEnd):
-        raise Exception('Invalid "}" character!')
-    elif isinstance(tokens[pos],Tokenizer.Column):
-        return (pos+1,Symbol(tokens[pos].text,tokens[pos].geometry))
+        raise Exception('Invalid ">" character!')
     else:
         raise Exception('Invalid syntax!')
