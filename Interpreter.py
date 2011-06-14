bd31a4c557fa51bdf05ee8191eca7daabd5be486
Horia Coman <horia141@gmail.com>
2011-06-15 00:34:15 +0300

    Factored function application into the "Func.apply" method.
    * I've factored function application into the "apply" method of Func.
      The interpreter code for the "Call" case has also been reworked. Now,
      if we're dealing with a function, it just computes the order and named
      arguments stored in the call object and invokes the "apply" method.
      Also, some verbose error reporting was refactored into smaller code.
    
    * We now have a new object, called "Callable", derived from "InAtom"
      from which "Func" and "BuiltIn" derive. Besides being cleaner (both
      "Func" and "BuiltIn" are types of callable objects) it provides a
      place to store the common "apply", "curry", "inject" code shared by
      both functions and builtins. For "apply", only the final step of the
      algorithm is customized by each derived class, by overriding the
      "_sp_apply" method. Similar work is/will be done for "__str__", "curry"
      etc.
    
    * I've fixed an issue when calling with named values and not all the
      required arguments are specified. The only tests I did were for proper
      lengths for the provided and required arguments. The problem arose
      when we used default named parameters. In the proper conditions (for
      example, two named parameters - one required and one optional, and just
      the optional one supplied) no errors were signaled. A "None" got into
      the new environment and chaos ensued. Assertion saved the day though.

--- Interpreter.py
+++ Interpreter.py
@@ -140,8 +140,8 @@ class FuncArg(object):
     def default(self):
         return self.__default
 
-class Func(InAtom):
-    def __init__(self,order,order_defs,order_var,named,named_defs,named_var,body,env):
+class Callable(InAtom):
+    def __init__(self,order,order_defs,order_var,named,named_defs,named_var):
         assert(isinstance(order,list))
         assert(all(map(lambda x: isinstance(x,FuncArg),order)))
         assert(isinstance(order_defs,list))
@@ -152,10 +152,6 @@ class Func(InAtom):
         assert(isinstance(named_defs,list))
         assert(all(map(lambda x: isinstance(x,FuncArg),named_defs)))
         assert(named_var == None or isinstance(named_var,str))
-        assert(isinstance(body,Parser.PsAtom))
-        assert(isinstance(env,dict))
-        assert(all(map(lambda x: isinstance(x,str),env.iterkeys())))
-        assert(all(map(lambda x: isinstance(x,InAtom),env.itervalues())))
 
         self.__order = map(lambda x: x.clone(),order)
         self.__order_defs = map(lambda x: x.clone(),order_defs)
@@ -163,8 +159,6 @@ class Func(InAtom):
         self.__named = map(lambda x: x.clone(),named)
         self.__named_defs = map(lambda x: x.clone(),named_defs)
         self.__named_var = str(named_var) if named_var else None
-        self.__body = body.clone()
-        self.__env = dict([(str(k),v.clone()) for (k,v) in env.iteritems()])
 
     def __str__(self):
         def spIfNNil(ls):
@@ -176,20 +170,24 @@ class Func(InAtom):
                      ' '.join(map(lambda x: str(x) + '!',self.__named)) + spIfNNil(self.__named) + \
                      ' '.join(map(lambda x: str(x) + '!',self.__named_defs)) + spIfNNil(self.__named_defs) + \
                      (str(self.__named_var) + '+ ' if self.__named_var else '') + \
-                     str(self.__body) + ']'
+                     str(self._sp_str()) + ']'
 
     def __repr__(self):
-        return 'Interpreter.Func(' + repr(self.__order) + ',' + \
-                                     repr(self.__order_defs) + ',' + \
-                                     repr(self.__order_var) + ',' + \
-                                     repr(self.__named) + ',' + \
-                                     repr(self.__named_defs) + ',' + \
-                                     repr(self.__named_var) + ',' + \
-                                     repr(self.__body) + ',' + \
-                                     repr(self.__env) + ')'
+        return 'Interpreter.Callable(' + repr(self.__order) + ',' + \
+                                         repr(self.__order_defs) + ',' + \
+                                         repr(self.__order_var) + ',' + \
+                                         repr(self.__named) + ',' + \
+                                         repr(self.__named_defs) + ',' + \
+                                         repr(self.__named_var) + ')'
 
     def __eq__(self,other):
         return False
+
+    def _sp_str(self):
+        raise Exception('Invalid call to "_sp_str" for Callable object!')
+
+    def _sp__apply(self,order,order_defs,order_var,named,named_defs,named_var):
+        raise Exception('Invalid call to "_sp_apply" for Callable object!')
 
     def apply(self,order_args,named_args):
         assert(isinstance(order_args,list))
@@ -255,15 +253,10 @@ class Func(InAtom):
         if self.hasNamedVar:
             named_var[self.__named_var] = Dict(named_var_kvs)
 
-        new_env = dict([(str(k),v.clone()) for (k,v) in self.__env.iteritems()])
-        new_env.update(order)
-        new_env.update(order_defs)
-        new_env.update(order_var)
-        new_env.update(named)
-        new_env.update(named_defs)
-        new_env.update(named_var)
-    
-        return interpret(self.__body,new_env,self)
+        if not all(map(lambda x: isinstance(x,InAtom),named.itervalues())):
+            raise Exception('Can\'t cover all named arguments!')
+
+        return self._sp_apply(order,order_defs,order_var,named,named_defs,named_var)
 
     def curry(self,va):
         pass
@@ -280,8 +273,7 @@ class Func(InAtom):
 
     def clone(self):
         return Func(self.__order,self.__order_defs,self.__order_var,
-                    self.__named,self.__named_defs,self.__named_var,
-                    self.__body,self.__env)
+                    self.__named,self.__named_defs,self.__named_var)
 
     @property
     def order(self):
@@ -315,6 +307,57 @@ class Func(InAtom):
     def namedVar(self):
         return self.__named_var
 
+class Func(Callable):
+    def __init__(self,order,order_defs,order_var,named,named_defs,named_var,body,env):
+        assert(isinstance(order,list))
+        assert(all(map(lambda x: isinstance(x,FuncArg),order)))
+        assert(isinstance(order_defs,list))
+        assert(all(map(lambda x: isinstance(x,FuncArg),order_defs)))
+        assert(order_var == None or isinstance(order_var,str))
+        assert(isinstance(named,list))
+        assert(all(map(lambda x: isinstance(x,FuncArg),named)))
+        assert(isinstance(named_defs,list))
+        assert(all(map(lambda x: isinstance(x,FuncArg),named_defs)))
+        assert(named_var == None or isinstance(named_var,str))
+        assert(isinstance(body,Parser.PsAtom))
+        assert(isinstance(env,dict))
+        assert(all(map(lambda x: isinstance(x,str),env.iterkeys())))
+        assert(all(map(lambda x: isinstance(x,InAtom),env.itervalues())))
+
+        super(Func,self).__init__(order,order_defs,order_var,named,named_defs,named_var)
+
+        self.__body = body.clone()
+        self.__env = dict([(str(k),v.clone()) for (k,v) in env.iteritems()])
+
+    def __repr__(self):
+        return 'Interpreter.Func(' + repr(self.order) + ',' + \
+                                     repr(self.orderDefs) + ',' + \
+                                     repr(self.orderVar) + ',' + \
+                                     repr(self.named) + ',' + \
+                                     repr(self.namedDefs) + ',' + \
+                                     repr(self.namedVar) + ',' + \
+                                     repr(self.__body) + ',' + \
+                                     repr(self.__env) + ')'
+
+    def _sp_str(self):
+        return str(self.__body)
+
+    def _sp_apply(self,order,order_defs,order_var,named,named_defs,named_var):
+        new_env = dict([(str(k),v.clone()) for (k,v) in self.__env.iteritems()])
+        new_env.update(order)
+        new_env.update(order_defs)
+        new_env.update(order_var)
+        new_env.update(named)
+        new_env.update(named_defs)
+        new_env.update(named_var)
+    
+        return interpret(self.__body,new_env,self)
+
+    def clone(self):
+        return Func(self.order,self.orderDefs,self.orderVar,
+                    self.named,self.namedDefs,self.namedVar,
+                    self.__body,self.__env)
+
     @property
     def body(self):
         return self.__body
@@ -323,7 +366,7 @@ class Func(InAtom):
     def env(self):
         return self.__env
 
-class BuiltIn(InAtom):
+class BuiltIn(Callable):
     def __init__(self,func):
         assert(hasattr(func,'__call__'))
 
@@ -394,63 +437,30 @@ class BuiltIn(InAtom):
             else:
                 raise Exception('Critical Error: Invalid BuiltIn FSM Path!')
 
-        self.__order = order
-        self.__order_var = order_var
-        self.__named = named
-        self.__named_var = named_var
+        super(BuiltIn,self).__init__(order,[],order_var,named,[],named_var)
+
         self.__func = func
-
-    def __str__(self):
-        def spIfNNil(ls):
-            return ' ' if len(ls) > 0 else ''
-
-        return '[' + ' '.join(map(str,self.__order)) + spIfNNil(self.__order) + \
-                     (str(self.__order_var) + '* ' if self.__order_var else '') + \
-                     ' '.join(map(str,self.__named)) + spIfNNil(self.__named) + \
-                     (str(self.__named_var) + '* ' if self.__named_var else '') + \
-                     '<<BuiltIn "' + self.__func.__name__ + '">>]'
 
     def __repr__(self):
         fullname = inspect.getmodule(self.__func).__name__ + '.' + self.__func.__name__
         return 'Interpreter.BuiltIn(' + fullname + ')'
 
-    def __eq__(self,other):
-        return False
+    def _sp__str(self):
+        return '<<BuiltIn "' + self.__func.__name__ + '">>'
+
+    def _sp_apply(self,order,order_defs,order_var,named,named_defs,named_var):
+        new_env = {}
+        new_env.update(order)
+        new_env.update(order_defs)
+        new_env.update(order_var)
+        new_env.update(named)
+        new_env.update(named_defs)
+        new_env.update(named_var)
+
+        return self.__func(**new_env)
 
     def clone(self):
         return BuiltIn(self.__func)
-
-    @property
-    def order(self):
-        return self.__order
-
-    @property
-    def orderDefs(self):
-        return []
-
-    @property
-    def hasOrderVar(self):
-        return self.__order_var != None
-
-    @property
-    def orderVar(self):
-        return self.__order_var
-
-    @property
-    def named(self):
-        return self.__named
-
-    @property
-    def namedDefs(self):
-        return []
-
-    @property
-    def hasNamedVar(self):
-        return self.__named_var != None
-
-    @property
-    def namedVar(self):
-        return self.__named_var
 
     @property
     def func(self):
@@ -543,144 +553,54 @@ def interpret(atom,env,curr_func):
     if isinstance(atom,Parser.Call):
         action = interpret(atom.action,env,curr_func)
 
-        if isinstance(action,Boolean):
-            raise Exception('Cannot call a boolean!')
-        elif isinstance(action,Number):
-            raise Exception('Cannot call a number!')
-        elif isinstance(action,Symbol):
+        if isinstance(action,Symbol):
             if action.value in env:
                 fn = env[action.value]
             else:
                 raise Exception('Couldn\'t find name "' + action.value + '"!')
-        elif isinstance(action,String):
-            raise Exception('Cannot call a string!')
-        elif isinstance(action,Func):
+        elif isinstance(action,Callable):
             fn = action
         elif isinstance(action,Dict):
             fn = action
         else:
-            raise Exception('Should not have gotten here!')
+            raise Exception('Cannot call object of that type!')
 
-        if isinstance(fn,Boolean):
-            if len(atom.orderArgs) == 0 and len(atom.namedArgs) == 0:
-                return fn
-            else:
-                raise Exception('Cannot apply arguments to a boolean!')
-        elif isinstance(fn,Number):
-            if len(atom.orderArgs) == 0 and len(atom.namedArgs) == 0:
-                return fn
-            else:
-                raise Exception('Cannot apply arguments to a number!')
-        elif isinstance(fn,Symbol):
-            if len(atom.orderArgs) == 0 and len(atom.namedArgs) == 0:
-                return fn
-            else:
-                raise Exception('Cannot apply arguments to a symbol!')
-        elif isinstance(fn,String):
-            if len(atom.orderArgs) == 0 and len(atom.namedArgs) == 0:
-                return fn
-            else:
-                raise Exception('Cannot apply arguments to a string!')
-        elif isinstance(fn,Func) or isinstance(fn,BuiltIn):
-            if len(atom.orderArgs) == 0 and len(atom.namedArgs) == 0:
-                return fn
-            else:
-                order = dict([(arg.name,arg.default) for arg in fn.order])
-                order_defs = dict([(arg.name,arg.default) for arg in fn.orderDefs])
-                order_var = {}
-                order_pos = 0
+        if len(atom.orderArgs) == 0 and len(atom.namedArgs) == 0:
+            return fn
 
-                named = dict([(arg.name,arg.default) for arg in fn.named])
-                named_defs = dict([(arg.name,arg.default) for arg in fn.namedDefs])
-                named_var = {}
-                named_var_kvs = []
-                named_pos = 0
+        if isinstance(fn,Callable):
+            order_args = []
+            named_args = {}
 
-                if len(atom.orderArgs) < len(order):
-                    raise Exception('Can\'t cover order arguments!')
+            for arg in atom.orderArgs:
+                order_args.append(interpret(arg,env,curr_func))
 
-                if len(atom.orderArgs) > len(order) + len(order_defs) and not fn.hasOrderVar:
-                    raise Exception('Too many order arguments!')
+            for arg in atom.namedArgs:
+                name = interpret(arg.name,env,curr_func)
 
-                if len(atom.namedArgs) < len(named):
-                    raise Exception('Can\'t cover named arguments!')
+                if not isinstance(name,Symbol):
+                    raise Exception('Named argument name isn\'t a Symbol!')
 
-                if len(atom.namedArgs) > len(named) + len(named_defs) and not fn.hasNamedVar:
-                    raise Exception('Too many named arguments!')
+                if name.value in named_args:
+                    raise Exception('Named argument "' + name.value + '" appears more than once!')
 
-                for arg in fn.order:
-                    order[arg.name] = interpret(atom.orderArgs[order_pos],env,curr_func)
-                    order_pos = order_pos + 1
+                named_args[name.value] = interpret(arg.value,env,curr_func)
 
-                for arg in fn.orderDefs:
-                    if order_pos < len(atom.orderArgs):
-                        order_defs[arg.name] = interpret(atom.orderArgs[order_pos],env,curr_func)
-                        order_pos = order_pos + 1
-                    else:
-                        break
-
-                if fn.hasOrderVar:
-                    var_kvs = [(Symbol('Length'),Number(len(atom.orderArgs) - order_pos))]
-
-                    for i in range(order_pos,len(atom.orderArgs)):
-                        var_kvs.append((Number(i - order_pos),interpret(atom.orderArgs[i],env,curr_func)))
-
-                    order_var[fn.orderVar] = Dict(var_kvs)
-
-                for arg in atom.namedArgs:
-                    name = interpret(arg.name,env,curr_func)
-
-                    if not isinstance(name,Symbol):
-                        raise Exception('Named argument name isn\'t Symbol!')
-
-                    # These cases are mutually exclusive and are the only four possibilities
-                    # given our assumptions about Call and Func.
-                    if name.value in named:
-                        named[name.value] = interpret(arg.value,env,curr_func)
-                    elif name.value in named_defs:
-                        named_defs[name.value] = interpret(arg.value,env,curr_func) 
-                    elif fn.hasNamedVar:
-                        named_var_kvs.append((Symbol(name.value),interpret(arg.value,env,curr_func)))
-                    else:
-                        raise Exception('Function does not have argument "' + name.value + '"!')
-
-                if fn.hasNamedVar:
-                    named_var[fn.namedVar] = Dict(named_var_kvs)
-                    
-                if isinstance(fn,Func):
-                    new_env = dict([(str(k),v.clone()) for (k,v) in fn.env.iteritems()])
-                    new_env.update(order)
-                    new_env.update(order_defs)
-                    new_env.update(order_var)
-                    new_env.update(named)
-                    new_env.update(named_defs)
-                    new_env.update(named_var)
-    
-                    return interpret(fn.body,new_env,fn)
-                else:
-                    new_env = {}
-                    new_env.update(order)
-                    new_env.update(order_defs)
-                    new_env.update(order_var)
-                    new_env.update(named)
-                    new_env.update(named_defs)
-                    new_env.update(named_var)
-
-                    return fn.func(**new_env)
+            return fn.apply(order_args,named_args)
         elif isinstance(fn,Dict):
-            if len(atom.orderArgs) == 0 and len(atom.namedArgs) == 0:
-                return fn
-            elif len(atom.orderArgs) == 1 and len(atom.namedArgs) == 0:
-                return fn.lookup(interpret(atom.orderArgs[0],env,curr_func))
+            if len(atom.orderArgs) == 1 and len(atom.namedArgs) == 0:
+                return fn.lookup(interpreter(atom.orderArgs[0],env,curr_func))
             else:
                 raise Exception('Cannot apply more than one argument to a dictionary!')
         else:
-            raise Exception('Should not have gotten here!')
+            raise Exception('Cannot apply arguments to an object of that type!')
     elif isinstance(atom,Parser.Boolean):
         if atom.text == '#T':
             return Boolean(True)
-        else:
+        elif atom.text == '#F':
             return Boolean(False)
+        else:
+            raise Exception('Critical Error: Invalid boolean value "' + atom.text + '"!')
     elif isinstance(atom,Parser.Number):
         return Number(float(atom.text))
     elif isinstance(atom,Parser.Symbol):
